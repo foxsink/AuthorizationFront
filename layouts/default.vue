b9014e0cb984eef732251737c18d664f297dac15
Yaroslav Karpov <iar.karpov@gmail.com>
2023-08-05 15:03:33 +0700

    Main page with sign in form in progress

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="h-full">
+        <header></header>
+
+        <main class="h-full">
+            <slot></slot>
+        </main>
+
+        <footer></footer>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    devtools: { enabled: true },
+    modules: [
+        '@nuxtjs/eslint-module',
+        '@nuxtjs/tailwindcss',
+    ],
+    tailwindcss: {
+        exposeConfig: true,
+    },
+})
