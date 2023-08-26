965fc7cbce827f5b763a5aadd9e258c14323b896
Yaroslav Karpov <iar.karpov@gmail.com>
2023-08-26 21:03:42 +0700

    Login/Registration form in progress. Some components have been added.

---
+++ components/baseComponents/baseButton/BaseButton.vue
@@ -0,0 +1,13 @@
+<template>
+    <button class="flex w-full justify-center rounded-md">
+        <slot />
+    </button>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+    @import 'styles/component';
+</style>
